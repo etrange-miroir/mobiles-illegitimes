63ba5338b74df06e1ca967dc04fc1014cd87e882
xseignard <xavier.seignard@gmail.com>
2013-11-08 18:55:04 +0100

    Added midi support

--- server/server.js
+++ server/server.js
@@ -1,20 +1,32 @@
 var util = require('util'),
 	SerialPort = require('serialport').SerialPort,
 	xbee_api = require('xbee-api'),
+	midi = require('midi')
+	output = new midi.output(),
 	chalk = require('chalk');
 
+// find the loopbe virtual midi port	
+for (var i = 0; i < output.getPortCount(); i++) {
+	if (output.getPortName(i).indexOf('LoopBe') > -1) {
+		output.openPort(i);
+		console.log(chalk.blue('[INFO] Connecté à l\'interface midi ' + output.getPortName(i)));
+	}
+};
+
+// conf serial port/xbee
 var xbeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2
 });
 
-var serialPort = new SerialPort('/dev/ttyUSB0', {
+var serialPort = new SerialPort('COM5', {
   parser: xbeeAPI.rawParser()
 });
 
 serialPort.on('open', function() {
-	console.log(chalk.blue('[INFO] Serveur connecté'));
+	console.log(chalk.blue('[INFO] Serveur démarré'));
 });
 
+// handle midi notes
 var processMessage = function(msg) {
 	var furnitureNumber = msg.substring(msg.lastIndexOf('i') + 1, msg.lastIndexOf('i') + 2);
 
@@ -24,14 +36,17 @@ var processMessage = function(msg) {
 	else if (msg.indexOf('close') > -1) {
 		var note = parseInt(furnitureNumber, 10);
 		console.log(chalk.green.bold('[NOTE] Meuble ' + furnitureNumber + ' : porte close, note midi:' + note));
+		output.sendMessage([176,note,1]);
 	}
 	else {
 		var door = msg.substring(msg.lastIndexOf('#') + 1, msg.length);
 		var note = parseInt(furnitureNumber, 10) * 10 + parseInt(door, 10);
 		console.log(chalk.green.bold('[NOTE] Meuble ' + furnitureNumber + ' : porte ' + door + ', note midi:' + note));
+		output.sendMessage([176,note,1]);
 	}
 };
 
+// intercept midi notes
 xbeeAPI.on('frame_object', function(frame) {
     var msg = '';
     frame.data.forEach(function(chunk) {
